9861475d8a049c46247f8373a9812d382fa45eea
zemersonnn <emersonzahab@gmail.com>
2024-01-28 18:06:31 -0600

    update

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -9,20 +9,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Use this linko tp get the GepJSON data
-let link_earthquake = " https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+// Use this link to get the GepJSON data
+var link_earthquake = " https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
 
 d3.json(link_earthquake, function(data) {
-       earthquake_function(data.features);
+    earthquakeFunction(data.features);
 });
 
-function earthquake_function(info) {
+function earthquakeFunction(info) {
 
-    var marker = [];
+var marker = [];
+
 
     for (var i = 0; i < info.length; i++) {
 
-        var mag = info[i].properties.mag
+        var magnitude = info[i].properties.mag
         var lat = info[i].geometry.coordinates[1]
         var lon = info[i].geometry.coordinates[0]
         var latlon = [lat, lon]
@@ -31,33 +32,36 @@ function earthquake_function(info) {
         var color = "";
 
         if (depth < 10){
-            color = "light pink"
+            color = "red"
         }
         else if (depth < 30) {
-            color = "dark pink"
+            color = "green"
         }
         else if (depth < 50) {
-            color = "purple"
+            color = "green"
         }
         else if (depth < 70) {
             color = "blue"
         }
         else if (depth < 90) {
-            color = "dark blue"
+            color = "blue"
         }
         else {
-            color = "black"
+            color = "purple"
         }
-    marker.push(
-        L.cirlce(latlon, {
-            stroke: false, 
-            fillOpacity: 0.5,
+
+        marker.push(
+        L.circle(latlon, {
+            stroke: false,
+            fillOpacity: 50,
             color: "white",
-            fillColor: color, 
-            radius: mag*100000
-        }).bindPopup("<h3" + info.properties.title + "</h3><hr><p>" + new Date(info[i].properties.time) + "</p>")
+            fillColor: color,
+            radius: magnitude*50000
+        }).bindPopup("<h3>" + info[i].properties.title +
+          "</h3><hr><p>" + new Date(info[i].properties.time) + "</p>")
     )
-    }
-    var earthquake = L.layerGroup(marker)
-    createImageBitmap(earthquake);
+  }
+  var earthquakes = L.layerGroup(marker)
+
+  createMap(earthquakes)
 }
