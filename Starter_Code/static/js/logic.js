3f3188ef185df742b0b331e517fbc0b3a520f459
zemersonnn <emersonzahab@gmail.com>
2024-02-01 21:02:23 -0600

    updated challenge 15

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,105 +1,109 @@
-// Creating the map object
-let myMap = L.map("map", {
-    center: [20.7128, -74.0059], 
-    zoom: 3
-}); 
-
-// Adding the tile layer 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-// Use this link to get the GepJSON data
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
-
-let mapStyle = {
-    color: "white",
-    fillColor: "pink",
-    fillOpacity: 0.5,
-    weight: 1.5 
-};
-
-d3.json(link, function(data) {
-    earthquakeFunction(data.features);
+// Create a tile layer 
+var defaultMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-function earthquakeFunction(info) {
+// Create basemap
+let basemap = { 
+    default: defaultMap
+};
 
-var marker = [];
+// Map object 
+var myMap = L.map("map", {
+    center: [35, -119],
+    zoom: 5,
+    layers: [defaultMap]
+});
 
+// Add default map to map 
+defaultMap.addTo(myMap);
 
-    for (var i = 0; i < info.length; i++) {
+// Earthquake variable
+let earthquake = new L.LayerGroup();
 
-        var magnitude = info[i].properties.mag
-        var lat = info[i].geometry.coordinates[1]
-        var lng = info[i].geometry.coordinates[0]
-        var latlng = [lat, lng]
-        var depth = info[i].geometry.coordinates[2]
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson")
+.then(function(data) {
+    L.geoJson(data,{
 
-        var color = "";
+        color: "yellow",
+        weight: 1
 
-        if (depth < 10){
-            color = "red"
+    }).addTo(earthquake);
+});
+
+// Add to map
+earthquake.addTo(myMap);
+
+// New variable
+earthquake_added = new L.LayerGroup();
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson").then(
+    function(earthquakes) {
+
+        // Console log 
+        console.log(earthquakes); 
+
+        // Make a function
+        function dataColor(depth) {
+            if (depth > 90)
+                return "red";
+            else if(depth > 70)
+                return "#fc4903";
+            else if(depth > 50)
+                return "#fc8403";
+            else if(depth > 30)
+                return "#fcad03";
+            else if(depth > 10)
+                return "#cafc03";
+            else
+                return "green";
         }
-        else if (depth < 30) {
-            color = "green"
-        }
-        else if (depth < 50) {
-            color = "green"
-        }
-        else if (depth < 70) {
-            color = "blue"
-        }
-        else if (depth < 90) {
-            color = "blue"
-        }
-        else {
-        
-            color = "purple"
-        }
-        
-        markers.push(marker);
-    }
-    createMap(L.layerGroup(markers));
 
-d3.json(link).then(
-    function(data) {
-        L.geoJson(data, {
-            style:function (feature) {
-                return {
-                    color: "white",
-                    fillColor: chooseColor(marker),
-                    fillOpacity: 0.5,
-                    weight: 1.5
-                };
-            }, 
-            // onEachFeature: function(feature, layer) {
-            //     layer.on({
+        function radiusSize(mag) {
+            if (mag==0)
+            return 1;
+            else 
+            return mag*5;
+        }
 
-            //     })
-            // }
+        function dataStyle(feature)
+        {
+            return { 
+              opacity: 0.5,
+              fillOpacity: 0.5,
+              fillColor: dataColor(feature.geometry.coordinates[2]), 
+              color: "000000", 
+              radius: radiusSize(feature.properties.mag), 
+              weight: 0.5,
+              stroke: true
+            }
+        }
 
-        }).addTo(myMap);
+        L.geoJson(earthquakes, {
+            
+            pointToLayer: function(feature, latLng) {
+                return L.circleMarker(latLng);
+            },
+            
+            style: dataStyle,
+           
+            onEachFeature: function(feature, layer){
+              layer.bindPopup(`Magnitude: <b>${feature.properties.mag}</b><br>
+                            Depth: <b>${feature.geometry.coordinates[2]}</b><br>
+                            Location: <b>${feature.properties.place}</b>`); 
+            }
+        }).addTo(earthquake_added);
+
     }
 );
-    //     marker.push(
-    //     L.circle(latlon, {
-    //         stroke: false,
-    //         fillOpacity: 50,
-    //         color: "white",
-    //         fillColor: color,
-    //         radius: magnitude*50000
-    //     }).bindPopup("<h3>" + info[i].properties.title +
-    //       "</h3><hr><p>" + new Date(info[i].properties.time) + "</p>")
-    // )
 
-var earthquakes = L.layerGroup(marker)
+earthquake_added.addTo(myMap);
 
-createMap(earthquakes);
-}
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson").then(marker);
+// Legend
+let legend = L.control({
+    position: "bottomright"
+});
 
-
-
-
-
+// Add to map
+legend.addTo(myMap);
